30a10394e75dea142326a572e26fb10cb6a86b7c
intanjuwitamanis <97723209+intanjuwitamanis@users.noreply.github.com>
2022-01-14 21:28:45 +0700

    Update index.php
    index telah di ubah

--- index.php
+++ index.php
@@ -54,7 +54,7 @@
     <div class="modal-dialog modal-md" role="document">
       <div class="modal-content">
         <div class="modal-header">
-        <h5 class="modal-title">Confirmation</h5>
+        <h5 class="modal-title">confirmation</h5>
       </div>
       <div class="modal-body">
         <div id="delete_content"></div>
