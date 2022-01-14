f505778be5ce00886572d41382e39ff418cb4e7d
Noviyanti99 <97726110+Noviyanti99@users.noreply.github.com>
2022-01-14 21:14:32 +0700

    Update about.php
    about telah diselesaikan

--- about.php
+++ about.php
@@ -3,7 +3,7 @@
             <div class="container h-100">
                 <div class="row h-100 align-items-center justify-content-center text-center">
                     <div class="col-lg-10 align-self-end mb-4" style="background: #0000002e;">
-                    	 <h1 class="text-uppercase text-white font-weight-bold">About Us</h1>
+                    	 <h1 class="text-uppercase text-white font-weight-bold">about Us</h1>
                         <hr class="divider my-4" />
                     </div>
                     
